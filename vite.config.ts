2e7dfae55ef67822669212d21a278865e4e781d3
Rafael Morais <rafahgmorais@gmail.com>
2026-01-31 23:13:47 -0300

    feat: add favicons

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    base: '/rafahgm.github.io/',
     plugins: [
         tailwindcss()
     ]
